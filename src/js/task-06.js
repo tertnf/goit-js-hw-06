3d233b695e8f1586b598fe463d3938e86e0cf3f5
Nataliia Tertyshna <tertnf@gmail.com>
2022-01-09 21:34:47 +0200

    Update task-06.js
    task 6 done

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -1,37 +1,24 @@
 const validationInputRef = document.querySelector("input, #validation-input");
 const inputLengthRef = document.querySelector("input, [data-length]");
 
-// console.log(validationInputRef.dataset.length);
-
-// validationInputRef.addEventListener("input", onValidationInputInput);
-
 validationInputRef.addEventListener("blur", onValidationInputBlur);
-
-// function onValidationInputInput(event) {
-//   //   console.log(event.currentTarget.value.length);
-//   let validationInputLength = event.currentTarget.value.length;
-//   return validationInputLength;
-// }
 
 function onValidationInputBlur(event) {
   if (
     Number(validationInputRef.dataset.length) ===
     event.currentTarget.value.length
   ) {
-    // console.log("grean");
     validationInputRef.classList.add("valid");
-  }
-  //   console.log("red");
-  else validationInputRef.classList.add("invalid");
+  } else validationInputRef.classList.add("invalid");
   //   console.log(validationInputRef.dataset.length);
-
   //   console.log(event.currentTarget.value.length);
-
-  //   console.log(
-  //     Number(validationInputRef.dataset.length) ===
-  //       event.currentTarget.value.length
-  //   );
 
   //   alert(typeof Number(validationInputRef.dataset.length));
   //   alert(typeof event.currentTarget.value.length);
+}
+validationInputRef.addEventListener("focus", onValidationInputFocus);
+
+function onValidationInputFocus(event) {
+  validationInputRef.classList.remove("valid");
+  validationInputRef.classList.remove("invalid");
 }
