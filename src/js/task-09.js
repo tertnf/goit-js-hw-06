2646cdfa7a726b33a127f5030e1ab40eddc3c594
Nataliia Tertyshna <tertnf@gmail.com>
2022-01-11 23:09:14 +0200

    Update task-09.js
    task 9 done

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const bodyRef = document.querySelector("body");
+const changeColorButtonRef = document.querySelector("button.change-color");
+const spanColorRef = document.querySelector("span.color");
+
+changeColorButtonRef.addEventListener("click", onChangeColorButtonClick);
+
+function onChangeColorButtonClick(event) {
+  bodyRef.style.backgroundColor = getRandomHexColor();
+  spanColorRef.textContent = getRandomHexColor();
+}
