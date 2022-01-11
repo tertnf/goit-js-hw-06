c115e2ca1a36bee70a80719fe018f5651fda6d7d
Nataliia Tertyshna <tertnf@gmail.com>
2022-01-11 22:55:13 +0200

    Update task-08.js
    task 8 done

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,16 @@
+const loginformRef = document.querySelector("form.login-form");
 
+loginformRef.addEventListener("submit", onLoginFormSubmit);
+
+function onLoginFormSubmit(event) {
+  event.preventDefault();
+  let object = {
+    email: event.currentTarget.elements.email.value,
+    password: event.currentTarget.elements.password.value,
+  };
+  if (object.email === "" || object.password === "") {
+    const alertMessage = "Все поля должы быть заполнены!";
+    alert(alertMessage);
+  } else console.log(object);
+  loginformRef.reset();
+}
